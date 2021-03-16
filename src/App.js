4e162e4c375de36940bc84c76152fe24b7de9650
Jared S <jaredjames2020@gmail.com>
2021-03-16 14:48:11 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { connect } from 'react-redux'; /* code change */
+=======
+import { connect } from 'react-redux';
+>>>>>>> 9af6cc2823643d6fdce1b0db4ea51c21cd043e59
 import './App.css';
-
+ 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.dispatch({
-      type: 'INCREASE_COUNT',
-    });
-  }
-
+  handleOnClick = event => {
+    this.props.increaseCount();
+  };
+ 
   render() {
     return (
       <div className="App">
+<<<<<<< HEAD
         <button onClick={() => this.handleOnClick()}>
           Click
         </button>
+=======
+        <button onClick={this.handleOnClick}>Click</button>
+>>>>>>> 9af6cc2823643d6fdce1b0db4ea51c21cd043e59
         <p>{this.props.items.length}</p>
       </div>
     );
   }
+}
+ 
+const mapStateToProps = state => {
+  return {
+    items: state.items
+  };
 };
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
+  };
+};
+<<<<<<< HEAD
 
 // start of code change
 const mapStateToProps = (state) => {
@@ -28,3 +46,10 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(App);
+=======
+ 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+>>>>>>> 9af6cc2823643d6fdce1b0db4ea51c21cd043e59
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+<<<<<<< HEAD
 import { Provider } from 'react-redux'; /* code change */
+=======
+import { Provider } from 'react-redux';
+>>>>>>> 9af6cc2823643d6fdce1b0db4ea51c21cd043e59
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
-
+ 
 const store = createStore(
   shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
+); /* code change */
+ 
 ReactDOM.render(
   <Provider store={store}>
     <App />
+<<<<<<< HEAD
   </Provider>, /* code change */
+=======
+  </Provider>,
+>>>>>>> 9af6cc2823643d6fdce1b0db4ea51c21cd043e59
   document.getElementById('root')
 );
